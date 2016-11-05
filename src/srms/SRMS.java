6d53ceaa8b2c3ded0e75c57e03ba2a798f2d7f54
arjun1321 <guptaarjun152@gmail.com>
2016-11-05 12:16:18 +0530

    Created login interface

--- src/srms/SRMS.java
+++ src/srms/SRMS.java
@@ -15,7 +15,12 @@ public class SRMS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
     }
     
 }
